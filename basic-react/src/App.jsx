91da59eaa82c8782970d44e07e798bb99f1bf5b9
kritika rathod <kritikarathod212@gmail.com>
2025-08-04 13:23:40 +0530

    first commit

---
+++ basic-react/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is App</h1>
+        <p>name</p>
+        <pri
+      </div>
+    );
+  }
+}
+
+export default App;
