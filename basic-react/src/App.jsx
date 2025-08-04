139bba721c73aaec31d35b31d5da46b0173db4b4
kritika rathod <kritikarathod212@gmail.com>
2025-08-04 14:41:25 +0530

    additing

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -9,25 +9,38 @@ class App extends React.Component {
     return (
       <div>
         <h1>This is App</h1>
-        <MenuItem name={"pizza"} price={200} disc={10}/>
-        <MenuItem name={"jgklj"}/>
+        <Comp1 data={"kritika"} />
       </div>
     );
-   
   }
 }
-  
-     function MenuItem(props) {
-      return(
-        <div>
-        <p>{props.name}</p>
-        <p>{props.price}</p>
-        <p>{props.disc}</p>
-        </div>
-      );
-      
-    }
-  
 
+function Comp1(props) {
+  return (
+    <div>
+      <h1>Component1</h1>
+      <Comp2 data={props.data} />
+    </div>
+  );
+}
+
+function Comp2(props) {
+  return (
+    <div>
+      <h1>Component2</h1>
+      <Comp3 data={props.data} />
+      <p>Data: {props.data}</p>
+    </div>
+  );
+}
+
+function Comp3(props) {
+  return (
+    <div>
+      <h1>Component3</h1>
+      <p>Data: {props.data}</p>
+    </div>
+  );
+}
 
 export default App;
